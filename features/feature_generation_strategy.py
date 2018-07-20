51bb4ac35ba40e1a3296e47fca942e6e2cded4fc
Jose Martin Rozanec <jmrozanec@gmail.com>
2018-07-20 11:37:15 -0300

    Implement avg, max, min strategies.

--- features/feature_generation_strategy.py
+++ features/feature_generation_strategy.py
@@ -56,3 +56,33 @@ class DivFeatureGenerationStrategy(FeatureGenerationStrategyAbstract):
 
     def equivalent_featurenames(self, colname1, colname2):
         return [featurename(colname1, colname2)]
+
+class AvgFeatureGenerationStrategy(FeatureGenerationStrategyAbstract):
+    def generate(self, df, colname1, colname2):
+        return df[featurename(colname1, colname2)]=df[[colname1, colname2]].mean(axis=1)
+
+    def featurename(self, colname1, colname2):
+        return "{}_avg_{}".format(colname1, colname2)
+
+    def equivalent_featurenames(self, colname1, colname2):
+        return [self.featurename(colname1, colname2), self.featurename(colname2, colname1)]
+
+class MaxFeatureGenerationStrategy(FeatureGenerationStrategyAbstract):
+    def generate(self, df, colname1, colname2):
+        return df[featurename(colname1, colname2)]=df[[colname1, colname2]].max(axis=1)
+
+    def featurename(self, colname1, colname2):
+        return "{}_max_{}".format(colname1, colname2)
+
+    def equivalent_featurenames(self, colname1, colname2):
+        return [self.featurename(colname1, colname2), self.featurename(colname2, colname1)]
+
+class MinFeatureGenerationStrategy(FeatureGenerationStrategyAbstract):
+    def generate(self, df, colname1, colname2):
+        return df[featurename(colname1, colname2)]=df[[colname1, colname2]].min(axis=1)
+
+    def featurename(self, colname1, colname2):
+        return "{}_min_{}".format(colname1, colname2)
+
+    def equivalent_featurenames(self, colname1, colname2):
+        return [self.featurename(colname1, colname2), self.featurename(colname2, colname1)]
